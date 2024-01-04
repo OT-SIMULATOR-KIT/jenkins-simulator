eca217814c21f1a1f2bdb788be5eaf4e8de50a4b
Sandeep <sandeep@opstree.com>
2024-01-04 23:00:35 +0530

    Added specific scripts for tasks

--- solution/test/scenario1/test.sh
+++ solution/test/scenario1/test.sh
@@ -3,7 +3,7 @@
 echo "TASK_NAME,TRIGGER_TYPE,TASK_SCRIPT_PATH" > ../../db/TaskMaster.csv
 
 pushd ../..
-./orchestrator.sh add task1 manual abc.sh
-./orchestrator.sh add task1 manual abc.sh
-./orchestrator.sh add task1 scheduled abc.sh
+./orchestrator.sh add task1 manual example1.sh
+./orchestrator.sh add task2 manual example1.sh
+./orchestrator.sh add task3 scheduled example2.sh
 popd 
